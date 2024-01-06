289fc4c256c1fc5d69ac70bd47001e32f350868e
Théo Balick <balickmethens@gmail.com>
2024-01-06 06:48:47 +0100

    Add Accordion component

---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,15 @@
+import Accordion from '../tailus/accordion/Accordion';
+import { ACCORDION_ITEM } from '../../constants/data.ts';
+
+export default () => {
+  return (
+    <Accordion.Root type="single" collapsible className="max-w-md w-full">
+      {ACCORDION_ITEM.map((item) => (
+        <Accordion.Item value={item.question} key={ACCORDION_ITEM.indexOf(item)}>
+          <Accordion.Trigger>{item.question}</Accordion.Trigger>
+          <Accordion.Content>{item.answer}</Accordion.Content>
+        </Accordion.Item>
+      ))}
+    </Accordion.Root>
+  );
+};
