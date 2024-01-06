b2157509d90170313a71e3bd1cc0daed5c1077f1
Théo Balick <balickmethens@gmail.com>
2024-01-06 06:48:26 +0100

    Add ACCORDION_ITEM constant to data.ts

---
+++ src/constants/data.ts
@@ -0,0 +1,19 @@
+export const ACCORDION_ITEM = [
+  {
+    id: 'item-1',
+    question: 'What is Tailus Themer?',
+    answer: 'Tailus Themer is a versatile theming library designed to help you create modern and custom web User Interfaces with Tailwind CSS.',
+  },
+  {
+    id: 'item-2',
+    question: 'What are the supported frameworks?',
+    answer:
+      'Tailus Themer is framework-agnostic and can be used with any front-end framework that supports Tailwind CSS, such as React, Vue, Angular, and others.',
+  },
+  {
+    id: 'item-3',
+    question: 'Is it customizable?',
+    answer:
+      'Yes, Tailus Themer is highly customizable. It allows you to customize all aspects of your theme, including colors, typography, and more. You can also export your theme configuration to use in other projects.',
+  },
+];
